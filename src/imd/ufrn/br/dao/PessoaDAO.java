bc3b8d530ff6b7452eab0e33aa998493a889b60d
JoabUrbano <joab.uaraujo@gmail.com>
2024-12-14 22:23:17 -0300

    PessoaDAO(REFACTORE): Added pessoa default to verification in for

--- src/imd/ufrn/br/dao/PessoaDAO.java
+++ src/imd/ufrn/br/dao/PessoaDAO.java
@@ -2,6 +2,8 @@ package imd.ufrn.br.dao;
 
 import imd.ufrn.br.controllers.GeradorImpostoRenda;
 import imd.ufrn.br.models.Pessoa;
+import imd.ufrn.br.models.SeguroVida;
+import imd.ufrn.br.models.ContaCorrente;
 
 import java.util.ArrayList;
 
@@ -9,9 +11,18 @@ public class PessoaDAO {
     GeradorImpostoRenda geradorImpostoRenda = new GeradorImpostoRenda();
     //@ non_null
     private ArrayList<Pessoa> pessoas;
+    //@ non_null
+    private ContaCorrente ccDefault;
+    //@ non_null
+    private SeguroVida svDefault;
+    //@ non_null
+    private Pessoa pDefault;
 
     private PessoaDAO() {
         this.pessoas = new ArrayList<Pessoa>();
+        this.ccDefault = new ContaCorrente("0000-0", "000.000-0", 1);
+        this.svDefault = new SeguroVida(1, 1, "Seguro Default", 1, 1);
+        this.pDefault = new Pessoa("Pessoa Default", 1, ccDefault, svDefault);
     }
 
     static private PessoaDAO instance;
@@ -44,7 +55,7 @@ public class PessoaDAO {
         return impostoTotal;
     }
     private Pessoa getMaiorPagador() {
-        Pessoa pessoa = null;
+        Pessoa pessoa = this.pDefault;
         for (Pessoa pp : pessoas) {
             double tributoTotal = geradorImpostoRenda.calculaValorTotalTributo(pp);
 
@@ -57,10 +68,10 @@ public class PessoaDAO {
     }
 
     private Pessoa getMaiorSeguro() {
-        Pessoa pessoa = null;
+        Pessoa pessoa = this.pDefault;
 
         for (Pessoa pp : pessoas) {
-            if (pessoa == null || pp.getSeguro().getValor() > pessoa.getSeguro().getValor()) {
+            if (pp.getSeguro().getValor() > pessoa.getSeguro().getValor()) {
                 pessoa = pp;
             }
         }
